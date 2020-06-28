eb4fc0d86ac8cec88fe735471787316c08eebe4f
abdul hamid <abdulachik@gmail.com>
2020-06-28 02:55:47 -0500

    updated base project

--- lib/suum_web/router.ex
+++ lib/suum_web/router.ex
@@ -16,7 +16,7 @@ defmodule SuumWeb.Router do
   scope "/", SuumWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
--- test/suum_web/controllers/page_controller_test.exs
+++ test/suum_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SuumWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Suum · Video Calls"
   end
 end

1b0317d1d75997a856542ad18bf64682c87b8c12
abdul hamid <abdulachik@gmail.com>
2020-10-10 16:22:35 -0500

    fixing deploy

--- config/releases.exs
+++ config/releases.exs
@@ -35,7 +35,7 @@ config :suum, SuumWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :suum, SuumWeb.Endpoint, server: true
+config :suum, SuumWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.

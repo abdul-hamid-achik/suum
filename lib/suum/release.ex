5a12eb6d130c1dd847f4fd26eb3a7453143869a2
abdul hamid <abdulachik@gmail.com>
2020-10-10 21:51:11 -0500

    fix

--- lib/suum/release.ex
+++ lib/suum/release.ex
@@ -2,7 +2,7 @@ defmodule Suum.Release.Tasks do
   @app :suum
 
   def migrate do
-    load_app()
+    start_app()
 
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
@@ -10,7 +10,7 @@ defmodule Suum.Release.Tasks do
   end
 
   def createdb do
-    load_app()
+    start_app()
 
     for repo <- repos() do
       :ok = ensure_repo_created(repo)
@@ -23,7 +23,7 @@ defmodule Suum.Release.Tasks do
   end
 
   def rollback(repo, version) do
-    load_app()
+    start_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
@@ -41,5 +41,11 @@ defmodule Suum.Release.Tasks do
 
   defp load_app do
     Application.load(@app)
+  end
+
+  defp start_app do
+    load_app()
+    Application.put_env(@app, :minimal, true)
+    Application.ensure_all_started(@app)
   end
 end

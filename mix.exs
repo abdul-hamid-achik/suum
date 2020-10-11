3d8f04e738f44c6a5e129a076c6cd25b7bda0806
abdul hamid <abdulachik@gmail.com>
2020-10-10 23:47:49 -0500

    adding ssl to extra

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Suum.MixProject do
   def application do
     [
       mod: {Suum.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 

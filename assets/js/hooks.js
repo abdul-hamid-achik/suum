0f7023ac199837444798a62addc8688fb19257bb
abdul hamid <abdulachik@gmail.com>
2020-10-13 02:34:03 -0500

    using only one webrtc server

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -33,17 +33,17 @@ function removeUserConnection(userUuid) {
 function createPeerConnection(lv, fromUser, offer) {
   let newPeerConnection = new RTCPeerConnection({
     iceServers: [
-      { urls: "stun:prod.suum.app:3478" },
-      { url: "stun:stun.1.google.com:19302" },
-      { url: "stun:stun1.1.google.com:19302" },
-      { url: "stun:stun2.1.google.com:19302" },
-      { url: "stun:stun3.1.google.com:19302" },
-      { url: "stun:stun4.1.google.com:19302" },
-      { url: "stun:stun.l.google.com:19302" },
-      { url: "stun:stun1.l.google.com:19302" },
-      { url: "stun:stun2.l.google.com:19302" },
-      { url: "stun:stun3.l.google.com:19302" },
-      { url: "stun:stun4.l.google.com:19302" },
+      // { urls: "stun:prod.suum.app:3478" },
+      { urls: "stun:stun.1.google.com:19302" },
+      // { url: "stun:stun1.1.google.com:19302" },
+      // { url: "stun:stun2.1.google.com:19302" },
+      // { url: "stun:stun3.1.google.com:19302" },
+      // { url: "stun:stun4.1.google.com:19302" },
+      // { url: "stun:stun.l.google.com:19302" },
+      // { url: "stun:stun1.l.google.com:19302" },
+      // { url: "stun:stun2.l.google.com:19302" },
+      // { url: "stun:stun3.l.google.com:19302" },
+      // { url: "stun:stun4.l.google.com:19302" },
     ]
   })
 

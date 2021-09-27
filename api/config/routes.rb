769c940838b53cd56883016d95ea836e5e9311c6
abdul hamid <abdulachik@gmail.com>
2021-09-27 10:49:48 -0500

    adding base and fixing typo

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_graphql_devise_for(User, { at: 'admin' })
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphiql_path: 'graphql#execute' if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
   mount Shrine.presign_endpoint(:cache) => '/s3/params'
 
   post '/graphql', to: 'graphql#execute'
